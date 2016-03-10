00a191171ca75408ac201fb8db4018ddef49f1b8
brunosmmm <brunosmmm@gmail.com>
2016-03-10 18:58:33 -0500

    Configurable AP parameters

--- scripts/enableap.sh
+++ scripts/enableap.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+source ./envsetup.sh
+source ./helpers.sh
+
+function log {
+    systemd_log "$1" periodicpi-configap
+}
+
+log 'Setting up Config mode AP'
+
+#get parameters
+ap_iface=$(extract_config $CONFIG_PATH/ap_config.json interface)
+ap_ip=$(extract_config $CONFIG_PATH/ap_config.json host_ip)
+
 #force wlan ip
-ifconfig wlan0 up 1.1.1.1 netmask 255.255.255.0
+ifconfig $ap_iface up $ap_ip netmask 255.255.255.0
 sleep 2
 
 #start dnsmasq
