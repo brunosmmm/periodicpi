b92d41f73b213edd4774da4adc376f03e546c0ad
brunosmmm <brunosmmm@gmail.com>
2016-03-10 14:25:44 -0500

    better daemon

--- tools/wifiscantool.py
+++ tools/wifiscantool.py
@@ -1,6 +1,32 @@
 import time
-import daemon
-from periodicpy.wifitools.scan import scan_and_report
+from daemon import runner
+from periodicpy.wifitools.scan import scan_and_parse
+from periodicpy.wifitools.wifiinfo import WifiInfoEncoder
+import json
 
-with daemon.DaemonContext():
-    scan_and_report()
+DEFAULT_SCAN_FILE = "/var/lib/periodicpi/wifi_scan.json"
+DEFAULT_SCAN_INTERVAL = 10
+
+class ScanWifi(object):
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/null'
+        self.stdout_path = '/dev/null'
+        self.pidfile_path = '/tmp/pwifi.pid'
+        self.pidfile_timeout = 5
+
+        self.scan_file = DEFAULT_SCAN_FILE
+        self.scan_interval = DEFAULT_SCAN_INTERVAL
+
+    def run(self):
+        while True:
+            scan_results = scan_and_parse('wlan0', True)
+
+            with open(self.scan_file, 'w') as scan_json:
+                scan_json.write(json.dumps(scan_results, cls=WifiInfoEncoder))
+
+            time.sleep(self.scan_interval)
+
+app = ScanWifi()
+daemon_runner = runner.DaemonRunner(app)
+daemon_runner.do_action()
