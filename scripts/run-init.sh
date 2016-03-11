9463b5b44e3382f8206edd5114a734c8412efdfc
brunosmmm <brunosmmm@gmail.com>
2016-03-11 09:01:08 -0500

    fixed bad variable usage

--- scripts/run-init.sh
+++ scripts/run-init.sh
@@ -11,11 +11,11 @@ log 'Entering run state'
 
 snapclient_state=$(extract_config $CONFIG_PATH/services.json snapclient)
 
-if [ snapclient_state == "True" ];
+if [ $snapclient_state == "True" ];
 then
     log 'Starting snapclient service...'
     snapclient_init=$(systemctl start snapclient)
-    if [ ! snapclient_init == 0 ];
+    if [ ! $snapclient_init == 0 ];
     then
         log 'Failed starting'
     else
