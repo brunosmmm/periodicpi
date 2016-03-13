369a88856708a2a6b74302e55eb7f2eb8dbb6c27
brunosmmm <brunosmmm@gmail.com>
2016-03-13 14:57:55 -0400

    just use python to perform initialization

--- scripts/run-init.sh
+++ scripts/run-init.sh
@@ -3,36 +3,5 @@
 source $PERIODIC_SCRIPT_PATH/envsetup.sh
 source $PERIODIC_SCRIPT_PATH/helpers.sh
 
-function log {
-    systemd_log "$1" periodicpi-run
-}
-
-log 'Entering run state'
-
-snapclient_state=$(extract_config $CONFIG_PATH/services.json snapclient)
-
-if [ $snapclient_state == "True" ];
-then
-    log 'Starting snapclient service...'
-    snapclient_init=$(systemctl start snapclient)
-    if [ ! $snapclient_init == 0 ];
-    then
-        log 'Failed starting'
-    else
-        log 'Success'
-    fi
-fi
-
-lircd_state=$(extract_config $CONFIG_PATH/services.json lircd)
-
-if [ $lircd_state == "True" ];
-then
-    log 'Starting lircd...'
-    lircd_init=$(systemctl start lircd)
-    if [ ! $lircd_init == 0 ];
-    then
-        log 'Failed starting'
-    else
-        log 'Success'
-    fi
-fi
+#just call python script
+python2 $PERIODIC_SCRIPT_PATH/../tools/node_init.py
---
+++ tools/node_init.py
@@ -0,0 +1,26 @@
+import json
+import subprocess
+from periodicpy.systemd.logging import log
+from periodicpy.systemd.control import start_service, ServiceStartStopError
+
+CONFIGURATION_PATH = '/etc/periodicpi'
+
+if __name__ == "__main__":
+
+    #read services
+    contents = {}
+    with open(CONFIGURATION_PATH+'/services.json', 'r') as f:
+        contents = json.load(f)
+
+    #enable services
+    for service in contents['services']:
+        if service['enabled']:
+            log('starting {} service'.format(service['service_name']), 'node-init')
+
+            try:
+                start_service(service['service_name'])
+                log('success', 'node-init')
+            except ServiceStartStopError:
+                log('faled starting', 'node-init')
+
+    log('startup done', 'node-init')
