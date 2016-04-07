631a23e9c634c7418f57f215e0e705d9680fec93
brunosmmm <brunosmmm@gmail.com>
2016-04-07 16:17:26 -0400

    updated lircpush plugin to new standards

--- plugins/lircpush/__init__.py
+++ plugins/lircpush/__init__.py
@@ -1,35 +1,26 @@
-from periodicpy.plugmgr.plugin import Module, ModuleArgument
-from periodicpy.plugmgr.plugin.method import ModuleMethod, ModuleMethodArgument
-from periodicpy.plugmgr.plugin.dtype import ModuleDataTypes
+from viscum.plugin import (Module,
+                           ModuleArgument)
 import os
 
-LIRC_CONFIG_PATH = '/usr/share/periodicpi/user_files/lircd/'
+LIRC_CONFIG_PATH = '/usr/share/periodicpi/user_files/lircd'
+
 
 class LircPusher(Module):
     _module_desc = ModuleArgument('lircpush', 'lirc configuration pusher')
-    _methods = {'push_remote_file' : ModuleMethod(method_desc='Push remote control description',
-                                                  method_args={'remote_name' : ModuleMethodArgument('Remote name',
-                                                                                                    required=True,
-                                                                                                    data_type=ModuleDataTypes.STRING),
-                                                               'file_contents' : ModuleMethodArgument('LIRC remote control description',
-                                                                                                      required=True,
-                                                                                                      data_type=ModuleDataTypes.STRING),
-                                                               'overwrite_file' : ModuleMethodArgument('Overwrite if already existent',
-                                                                                                       required=False,
-                                                                                                       data_type=ModuleDataTypes.BOOLEAN)},
-                                                  method_return=ModuleDataTypes.BOOLEAN)}
 
     def __init__(self, **kwargs):
         super(LircPusher, self).__init__(**kwargs)
 
         self._automap_methods()
 
-    def _push_remote_file(self, remote_name, file_contents, overwrite_file=False):
+    def _push_remote_file(self, remote_name,
+                          file_contents, overwrite_file=False):
 
-        remote_file = LIRC_CONFIG_PATH+remote_name+'.conf'
+        remote_file = os.path.join(LIRC_CONFIG_PATH,
+                                   remote_name+'.conf')
 
         if os.path.isfile(remote_file):
-            if overwrite_file == False:
+            if overwrite_file is False:
                 return False
 
         file_written = False
@@ -37,10 +28,16 @@ class LircPusher(Module):
             f.write(file_contents)
             file_written = True
 
-        if file_written == False:
+        if file_written is False:
             return False
 
         return True
 
+
 def discover_module(**kwargs):
-    return LircPusher
+    class LircPusherProxy(LircPusher):
+        _, _methods, _propertues =\
+            Module.build_module_structure_from_file(os.path.join(kwargs['plugin_path'],
+                                                                 'lircpush,json'))
+
+    return LircPusherProxy
